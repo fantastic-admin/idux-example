d223174eba596424c80a97e95f0c02b642550770
Hooray Hu <304327508@qq.com>
2023-12-21 02:33:45 +0800

    迁移到 4.3.0

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,6 @@ import '@/utils/baidu'
 
 import '@/utils/system.copyright'
 
-import IduxCdk from '@idux/cdk'
-import IduxComponents from '@idux/components'
-import { IDUX_ICON_DEPENDENCIES, addIconDefinitions } from '@idux/components/icon'
-import { createGlobalConfig } from '@idux/components/config'
-import { zhCN } from '@idux/components/locales'
-import '@idux/components/index.full.css'
-
 import FloatingVue from 'floating-vue'
 import 'floating-vue/dist/style.css'
 
@@ -20,6 +13,7 @@ import 'overlayscrollbars/overlayscrollbars.css'
 import App from './App.vue'
 import pinia from './store'
 import router from './router'
+import ui from './ui-provider'
 
 // 自定义指令
 import directive from '@/utils/directive'
@@ -37,14 +31,13 @@ import 'virtual:uno.css'
 import '@/assets/styles/globals.scss'
 
 const app = createApp(App)
-addIconDefinitions(IDUX_ICON_DEPENDENCIES)
-app.use(IduxCdk).use(IduxComponents).use(createGlobalConfig({ locale: zhCN }))
 app.use(FloatingVue, {
   distance: 12,
 })
 app.use(Message)
 app.use(pinia)
 app.use(router)
+app.use(ui)
 directive(app)
 if (icons.isOfflineUse) {
   for (const info of icons.collections) {
